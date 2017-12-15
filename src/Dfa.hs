8e039b46fd62b3e43d3b5d7440a7cb239b4bb38c
Christopher Grossack <HallaSurvivor@gmail.com>
2017-12-15 01:30:24 -0500

    finished?

--- src/Dfa.hs
+++ src/Dfa.hs
@@ -1,54 +1,87 @@
-module Dfa where
+{-# LANGUAGE ExistentialQuantification #-}
+module Dfa 
+  (Bit
+  ,DFA
+  ,mkDFA
+  ,nonEmpty
+  ,intersect
+  ,union
+  ,complement
+  ,eliminate
+  ) where
 
 import Prelude hiding (init)
 
 data Bit = Z | O
 
 -- | arbirtary arity DFA over {0,1} with states coming from s
-data DFA s = DFA { transition :: [Bit] -> s -> s
-                 , init       :: s
-                 , final      :: s -> Bool 
-                 }
+data DFAint s = DFAint { transition :: [Bit] -> s -> s
+                       , init       :: s
+                       , final      :: s -> Bool 
+                       }
 
 -- | Checks if a DFA accepts at least one word (requires the DFA be of arity 0)
-nonEmpty :: (Eq s) => DFA s -> Bool
-nonEmpty d = dfs (init d) (const False)
+nonEmpty' :: (Eq s) => DFAint s -> Bool
+nonEmpty' d = dfs (init d) (const False)
   where
     dfs q seen = not (seen q) && (final d q || dfs q' (\x -> x == q || seen x))
       where
         q' = transition d [] q
 
 -- | DFA intersection (propositional AND)
-intersect :: DFA s -> DFA t -> DFA (s,t)
-intersect d1 d2 = DFA delta q0 f
+intersect' :: DFAint s -> DFAint t -> DFAint (s,t)
+intersect' d1 d2 = DFAint delta q0 f
   where
     q0    = (init d1, init d2)
     f     = \(q1,q2) -> final d1 q1 && final d2 q2
     delta = \bs (q1, q2) -> (transition d1 bs q1, transition d2 bs q2)
 
 -- | DFA disjoint union (propositional OR)
-union :: DFA s -> DFA t -> DFA (s,t)
-union d1 d2 = DFA delta q0 f
+union' :: DFAint s -> DFAint t -> DFAint (s,t)
+union' d1 d2 = DFAint delta q0 f
   where
     q0    = (init d1, init d2)
     f     = \(q1,q2) -> final d1 q1 || final d2 q2
     delta = \bs (q1, q2) -> (transition d1 bs q1, transition d2 bs q2)
 
 -- | DFA complement (propositional NOT)
-complement :: DFA s -> DFA s
-complement d = DFA delta q0 f
+complement' :: DFAint s -> DFAint s
+complement' d = DFAint delta q0 f
   where
     q0    = init d
     f     = not . final d
     delta = transition d
 
 -- | DFA track elimination (propositional EXISTS)
-eliminate :: Int -> DFA s -> DFA [s] 
-eliminate n d = DFA delta q0 f
+eliminate' :: DFAint s -> DFAint [s] 
+eliminate' d = DFAint delta q0 f
   where
     q0    = [init d]
     f     = not . null . filter (final d) 
     delta bs qs = [transition d bs0 q | q <- qs] ++ [transition d bs1 q | q <- qs]
       where
-        bs0 = take n bs ++ Z:drop n bs
-        bs1 = take n bs ++ O:drop n bs
+        bs0 = Z:tail bs
+        bs1 = O:tail bs
+
+
+-- Exposed!
+
+data DFA = forall s . Eq s => DFA (DFAint s)
+
+mkDFA :: (Eq s) => s -> ([Bit] -> s -> s) -> (s -> Bool) -> DFA
+mkDFA q0 delta f = DFA $ DFAint delta q0 f
+
+nonEmpty :: DFA -> Bool
+nonEmpty (DFA d) = nonEmpty' d
+
+intersect :: DFA -> DFA -> DFA
+intersect (DFA d1) (DFA d2) = DFA $ intersect' d1 d2
+
+union :: DFA -> DFA -> DFA
+union (DFA d1) (DFA d2) = DFA $ union' d1 d2
+
+complement :: DFA -> DFA
+complement (DFA d) = DFA $ complement' d
+
+eliminate :: DFA -> DFA
+eliminate (DFA d) = DFA $ eliminate' d
--- src/Structure.hs
+++ src/Structure.hs
@@ -2,4 +2,25 @@ module Structure where
 
 import qualified Dfa
 
+type Structure = Char -> Dfa.DFA
 
+data Predicate = Atom Char
+               | And Predicate Predicate
+               | Or  Predicate Predicate
+               | Not Predicate
+               | Exists Predicate
+               | ForAll Predicate
+
+-- Requires the input have all quantififers out front, 
+-- with the innermost quantifier corresponding to the topmost tape,
+-- second quantifier corresponding to the second tape, ...
+-- outermost quantifier corresponding to the bottommost tape
+eval :: Structure -> Predicate -> Bool
+eval getRel p = Dfa.nonEmpty $ eval' getRel p
+  where
+    eval' getRel (Atom c)    = getRel c
+    eval' getRel (And p1 p2) = Dfa.intersect  (eval' getRel p1) (eval' getRel p2)
+    eval' getRel (Or  p1 p2) = Dfa.union      (eval' getRel p1) (eval' getRel p2)
+    eval' getRel (Not p)     = Dfa.complement (eval' getRel p)
+    eval' getRel (Exists p)  = Dfa.eliminate  (eval' getRel p)
+    eval' getRel (ForAll p)  = Dfa.complement $ Dfa.eliminate $ Dfa.complement (eval' getRel p)
